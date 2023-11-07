7ae7b526227ea351b541969fce3f43da78f6a1c6
Raza Jafri <razajafri@users.noreply.github.com>
2023-11-07 12:57:08 -0800

    added databricks provider

--- delta-lake/common/src/main/databricks/scala/com/nvidia/spark/rapids/delta/DatabricksDeltaProvider.scala
+++ delta-lake/common/src/main/databricks/scala/com/nvidia/spark/rapids/delta/DatabricksDeltaProvider.scala
@@ -340,11 +340,13 @@ class DeltaCreatableRelationProviderMeta(
   override def convertToGpu(): GpuCreatableRelationProvider = new GpuDeltaDataSource(conf)
 }
 
-/**
- * Implements the Delta Probe interface for probing the Delta Lake provider on Databricks.
- * @note This is instantiated via reflection from ShimLoader.
- */
-class DeltaProbeImpl extends DeltaProbe {
-  // Delta Lake is built-in for Databricks instances, so no probing is necessary.
-  override def getDeltaProvider: DeltaProvider = DatabricksDeltaProvider
-}
+//object DatabricksDeltaProvider extends DatabricksDeltaProviderBase
+//
+///**
+// * Implements the Delta Probe interface for probing the Delta Lake provider on Databricks.
+// * @note This is instantiated via reflection from ShimLoader.
+// */
+//class DeltaProbeImpl extends DeltaProbe {
+//  // Delta Lake is built-in for Databricks instances, so no probing is necessary.
+//  override def getDeltaProvider: DeltaProvider = DatabricksDeltaProvider
+//}
